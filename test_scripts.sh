94a65a6a49e12bb5105141b9770c70c5e637dd56
Ulises Rosas <ulisesrosas@Arapaima.attlocal.net>
2024-08-10 18:55:39 -0500

    straighter example

--- test_scripts.sh
+++ test_scripts.sh
@@ -1,7 +1,7 @@
 
 ./scripts/sim_nets.R 15 --out_path ./test_data/test_sims --prefix test --ncores 2 --max_iter 50
 
-./scripts/infer_qlls.jl ./test_data/1_seqgen.CFs_n15.csv ./test_data/test_sims/test2*.txt\
+./scripts/infer_qlls.jl ./test_data/1_seqgen.CFs_n15.csv ./test_data/test_sims/test*.txt\
                                   --ncores 2 --outfile ./test_data/test_n15_qll.csv
 
 ./src/path_subsampling.py ./test_data/test_n15_qll.csv ./test_data/1_seqgen.CFs_n15.csv\
@@ -13,14 +13,16 @@
     --prefix ./test_data/non_linear_batches
 
 
+# tail -n 1 ./test_data/linear_batches_overlappedBatches.txt  > ./test_data/linear_batches_overlappedBatches_best.txt
 
-
+# 4 mins
 ./scripts/infer_nets_batches.jl ./test_data/1_seqgen.QMC_n15.tre\
                         ./test_data/1_seqgen.CFs_n15.csv\
-                        ./test_data/non_linear_batches_overlappedBatches_isle.txt\
-                        --h_max 2 --nruns 2 --Nfail 15\
-                        --prefix non_linear_overlapped \
+                        ./test_data/linear_batches_overlappedBatches.txt\
+                        --h_max 2 --nruns 2 --Nfail 5\
+                        --prefix linear_overlapped \
                         --ncores 2 
+
 
 # ./infer_nets_batches.jl 1_seqgen.QMC_n15.tre\
 #                         1_seqgen.CFs_n15.csv\
