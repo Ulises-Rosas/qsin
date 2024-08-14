07c38687dd6beef08b06295ed5715bbc7092c61e
Ulises Rosas <ulisesrosas@Arapaima.attlocal.net>
2024-08-14 11:05:52 -0500

    relying on conda package for non-julia deps

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 julia -e 'using Pkg; Pkg.add("Suppressor")'
-julia -e 'using Pkg, Suppressor; println("Installing DataFrames"); @suppress Pkg.add("DataFrames"); Pkg.add("CSV"); Pkg.add("PhyloNetworks")'
-Rscript -e 'install.packages("SiPhyNetwork", repos="https://cloud.r-project.org")'
+# julia -e 'using Pkg, Suppressor; println("Installing DataFrames"); @suppress Pkg.add("DataFrames"); Pkg.add("CSV"); Pkg.add("PhyloNetworks")'
+# Rscript -e 'install.packages("SiPhyNetwork", repos="https://cloud.r-project.org")'
 
+julia -e 'using Pkg, Suppressor; println("Installing PhyloNetworks"); @suppress Pkg.add("PhyloNetworks")'
 
 echo
 echo -e "\033[7;32""m qsin installation complete \033[0m"
