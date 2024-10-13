a5adea94c566b038fe9e0192725cdc076205285e
Ulises Rosas <ulisesfrosasp@gmail.com>
2024-10-13 18:50:25 -0500

    post net analysis

--- test_data/analysis/post_net.sh
+++ test_data/analysis/post_net.sh
@@ -1,41 +1,38 @@
 
-# full_data="./test_data/full_data_net15.txt"
-# inferd_nets_loc="/Users/ulisesrosas/Desktop/qsin/test_data/n15/n15_linear"
-
-
-# full_data="./test_data/full_data_net15.txt"
-# inferd_nets_loc="/Users/ulisesrosas/Desktop/qsin/test_data/n15/n15_linear"
-# log_file=$inferd_nets_loc/linear_nets.log
-
-# ./test_data/analysis/compare_nets.jl $full_data\
-#                   $inferd_nets_loc/*_nets.txt\
-#                   --outfile $inferd_nets_loc/compared_nets_linear.csv\
-#                   --root 15
-
-# ./test_data/analysis/process_time.py $log_file\
-#                   -o $inferd_nets_loc/processed_time_linear.csv
-
-
-# conda activate  /Users/ulisesrosas/miniconda3/envs/qsin 
-
 full_data="./test_data/full_data_net15.txt"
-inferd_nets_loc="/Users/ulisesrosas/Desktop/qsin/test_data/n15_depth3_v0.9_n0.5"
-log_file=$inferd_nets_loc/nonlinear_depth3_nu0.9_nets_15more.log
+inferd_nets_loc="/Users/ulises/Desktop/ABL/software/qsin/test_data/n15_non_linear_oscer"
+
+log_file=$(ls $inferd_nets_loc/*.log)
+
+# get base name of $inferd_nets_loc
+inferd_nets_loc_base=$(basename $inferd_nets_loc)
 
 ./test_data/analysis/compare_nets.jl $full_data\
                   $inferd_nets_loc/*_nets.txt\
-                  --outfile $inferd_nets_loc/compared_nets_n15_depth3_v0.9_n0.5.csv\
-                  --root 15
-
+                  --outfile $inferd_nets_loc/compared_nets_$inferd_nets_loc_base.csv\
+                  --root 12
+# 12, 9 
 ./test_data/analysis/process_time.py $log_file\
-                  -o $inferd_nets_loc/processed_time_n15_depth3_v0.9_n0.5.csv
+                  -o $inferd_nets_loc/processed_time_$inferd_nets_loc_base.csv
+
+./test_data/analysis/process_pseudodeviances.py $inferd_nets_loc/*liks.txt\
+                  -o $inferd_nets_loc/processed_pseudo_$inferd_nets_loc_base.csv
 
 
-ls $inferd_nets_loc/compared_nets_n15_depth3_v0.9_n0.5.csv
-ls $inferd_nets_loc/*uniq.txt
-ls $inferd_nets_loc/processed_time_n15_depth3_v0.9_n0.5.csv
+echo
+echo
+
+echo file = "'$inferd_nets_loc/compared_nets_$inferd_nets_loc_base.csv'"
+echo combs_file = "'$(ls $inferd_nets_loc/*uniq.txt)'"
+echo time_file ="'$inferd_nets_loc/processed_time_$inferd_nets_loc_base.csv'"
+echo pseudo_file ="'$inferd_nets_loc/processed_pseudo_$inferd_nets_loc_base.csv'"
+
+echo
+echo
+
+error_file=$(ls $inferd_nets_loc/*testErrors.csv)
+path_file=$(ls $inferd_nets_loc/*elnetPath.csv)
 
 
-
-ls $inferd_nets_loc/*testErrors.csv
-ls $inferd_nets_loc/*elnetPath.csv
+echo 'errors_path = np.loadtxt("'$error_file'", delimiter="," )'
+echo 'elnet_path  = np.loadtxt("'$path_file'", delimiter="," )'
