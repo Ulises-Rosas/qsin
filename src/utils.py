da130e52a4108623d59bc688fd2e51976f3a6f09
Ulises Rosas <ulisesfrosasp@gmail.com>
2025-03-16 19:25:50 -0500

    standarize utils

--- src/utils.py
+++ src/utils.py
@@ -97,3 +97,48 @@ def progressbar(it, prefix="", size=60, out=sys.stdout):
         yield item
         show(i+1)
     print("\n", flush=True, file=out)
+
+
+def standardize_Xy(X_train, y_train, X_test = None, y_test = None, nstdy = False):
+    """
+    Standardize the data
+    """
+    x_u = np.mean(X_train, axis = 0)
+    x_sd = np.std(X_train, axis = 0)
+    
+    zero_sd_indices = x_sd == 0 # O(p)
+    if np.any(zero_sd_indices): # O(p)
+        x_sd[zero_sd_indices] = 1 # O(p)
+
+    y_u = np.mean(y_train)
+    y_sd = np.std(y_train)
+
+    if y_sd == 0:
+        y_sd = 1
+
+    X_train = (X_train - x_u) / x_sd
+    y_train = y_train - y_u if nstdy else (y_train - y_u) / y_sd
+
+    if X_test is not None:
+        X_test = (X_test - x_u) / x_sd
+        y_test = y_test - y_u if nstdy else (y_test - y_u) / y_sd
+
+    return X_train, X_test, y_train, y_test
+
+def standardize_X(X_train,  X_test = None):
+    """
+    Standardize the data
+    """
+    x_u = np.mean(X_train, axis = 0)
+    x_sd = np.std(X_train, axis = 0)
+    
+    x_sd_zero = x_sd == 0
+    if np.any(x_sd_zero):
+        x_sd[x_sd_zero] = 1
+
+    X_train = (X_train - x_u) / x_sd
+
+    if X_test is not None:
+        X_test = (X_test - x_u) / x_sd
+
+    return X_train, X_test
