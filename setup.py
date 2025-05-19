11f18c454127c5aa310d52fef1b6bf24cd3fb722
Ulises Rosas <ulisesfrosasp@gmail.com>
2025-05-19 15:25:34 -0500

    bump version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('README.md') as readme_file:
     readme = readme_file.read()
 
 setup(name = "qsin",
-      version = '0.10.1',
+      version = '0.11.1',
       maintainer = 'Ulises Rosas',
       packages = ['qsin'],
       package_dir = {'qsin': 'src'},
